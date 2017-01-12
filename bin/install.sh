e998bcbbbfdcb27ea63d5c072935a76e2099773a
shengrong1987 <aimbebe.r@gmail.com>
2017-01-12 08:42:18 +0800

    Not reinstall Sails-hook-jobs during setup

--- bin/install.sh
+++ bin/install.sh
@@ -2,11 +2,9 @@
 npm uninstall bcrypt
 npm uninstall forever
 npm uninstall grunt
-npm uninstall sails-hook-jobs
 npm uninstall mimer
 npm uninstall es5-ext
 npm uninstall moment-timezone
-npm install sails-hook-jobs
 npm install -g grunt --save
 npm install
 npm install --save sails-hook-parametized-policies
