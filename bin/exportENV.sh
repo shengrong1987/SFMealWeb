374ba6da04035b5aeb2a9fe1fc2e834fa0e2ca90
shengrong1987 <aimbebe.r@gmail.com>
2017-01-26 22:24:56 +0800

    separate export ENV script

---
+++ bin/exportENV.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+get_instance_tags(){
+   instance_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
+   echo $(aws ec2 describe-tags --region "us-west-2" --filters "Name=resource-id,Values=$instance_id")
+}
+tags_to_env () {
+    tags=$1
+
+    echo $tags
+
+    for key in $(echo $tags | /usr/bin/jq/jq -r ".[][].Key"); do
+        value=$(echo $tags | /usr/bin/jq/jq -r ".[][] | select(.Key==\"$key\") | .Value")
+        key=$(echo $key | /usr/bin/tr '-' '_' | /usr/bin/tr '[:lower:]' '[:upper:]')
+        echo 'export $key="$value"' >> /home/ec2-user/.bashrc
+    done
+}
+instance_tags=$(get_instance_tags)
+tags_to_env "$instance_tags"
--- bin/install.sh
+++ bin/install.sh
@@ -12,22 +12,3 @@ sudo chmod -R 0755 ./data
 sudo chown -R $USER ./data
 sudo chmod -R 501 ./.tmp
 sudo chmod -R 501 ./views
-get_instance_tags(){
-   instance_id=$(/usr/bin/curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-   echo $(aws ec2 describe-tags --region "us-west-2" --filters "Name=resource-id,Values=$instance_id")
-}
-tags_to_env () {
-    tags=$1
-
-    echo tags
-
-    for key in $(echo $tags | /usr/bin/jq/jq -r ".[][].Key"); do
-        value=$(echo $tags | /usr/bin/jq/jq -r ".[][] | select(.Key==\"$key\") | .Value")
-        key=$(echo $key | /usr/bin/tr '-' '_' | /usr/bin/tr '[:lower:]' '[:upper:]')
-        echo $key
-        echo $value
-        export $key="$value"
-    done
-}
-instance_tags=$(get_instance_tags)
-tags_to_env "$instance_tags"
