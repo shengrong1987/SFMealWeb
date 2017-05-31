323fe613ec325308f9673a129090dcd71b863314
shengrong1987 <aimbebe.r@gmail.com>
2017-05-31 02:28:52 -0700

    remove specific explicitHost

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-   port: 80,
+   port: 80
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
@@ -35,6 +35,6 @@ module.exports = {
   //   level: "silent"
   // }
 
-  explicitHost : "ec2-52-43-218-65.us-west-2.compute.amazonaws.com"
+  // explicitHost : "ec2-52-43-218-65.us-west-2.compute.amazonaws.com"
 
 };
