d47c10e523ba863a4d8e6c742bdac0dce2c8b742
shengrong1987 <aimbebe.r@gmail.com>
2017-06-04 11:00:44 -0700

    Optimize start server process and add logging

--- bin/install.sh
+++ bin/install.sh
@@ -5,10 +5,3 @@ npm uninstall es5-ext
 npm install -g grunt --save
 npm install
 npm install --save sails-hook-parametized-policies
-sudo grunt prod
-sudo chmod -R 0755 ./data
-sudo chown -R $USER ./data
-sudo chmod -R 701 ./.tmp
-sudo chown -R 501 ./.tmp
-sudo chmod -R 701 ./views
-sudo chown -R 501 ./views
