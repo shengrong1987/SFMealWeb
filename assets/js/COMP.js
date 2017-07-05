6dbcf6448e91edd3b4eb0773ad456f74be401a35
shengrong1987 <aimbebe.r@gmail.com>
2017-07-04 17:12:49 -0700

    Fix datetimepickuper in confirm page in party mode

--- assets/js/COMP.js
+++ assets/js/COMP.js
@@ -817,13 +817,15 @@
   $(window).on('load',function(){
     $('[data-toggle="dateTimePicker"]').each(function(){
       var dateString = $(this).data("date");
+      var minDate;
       if(typeof dateString != "undefined" && dateString != "undefined"){
         var date = new Date(dateString);
         var mDate = moment(date.toISOString());
-        var minDate = $(this).data("min");
         // if(mDate.isBefore(moment()) && $(this).data("min") == "now"){
         //   mDate = moment();
         // }
+      }else{
+        minDate = $(this).data("min");
       }
       $(this).datetimepicker({
         icons:{
