8a13aae2ada90c00c839eb6b2f5cc9292eddcb35
shengrong1987 <aimbebe.r@gmail.com>
2018-04-25 02:10:22 -0700

    Update grunt startup script

--- bin/install.sh
+++ bin/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-npm uninstall grunt
 npm uninstall mimer
 npm uninstall es5-ext
-npm install -g grunt --save
+npm install -g grunt-cli
+npm install -g grunt --save-dev
 npm install
 npm install --save sails-hook-parametized-policies
 sudo chown -R $USER:$USER .
