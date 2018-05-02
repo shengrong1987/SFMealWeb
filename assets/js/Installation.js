37d663c1345d9084945dc35b66a1842c164eeefb
shengrong1987 <aimbebe.r@gmail.com>
2018-05-02 01:07:35 -0700

    Add hashtag cleanup

--- assets/js/Installation.js
+++ assets/js/Installation.js
@@ -32,6 +32,28 @@ function initData(){
   getCountyInfo();
   loadOrder(true);
   loadPreference();
+  initHashTag();
+}
+
+function initHashTag(){
+  if (window.location.hash === '#_=_' || (window.location.href.indexOf("#") !== -1 && window.location.href.split("#")[1] === "")){
+
+    // Check if the browser supports history.replaceState.
+    if (history.replaceState) {
+
+      // Keep the exact URL up to the hash.
+      var cleanHref = window.location.href.split('#')[0];
+
+      // Replace the URL in the address bar without messing with the back button.
+      history.replaceState(null, null, cleanHref);
+
+    } else {
+
+      // Well, you're on an old browser, we can get rid of the _=_ but not the #.
+      window.location.hash = '';
+
+    }
+  }
 }
 
 function initEventHandler(){
